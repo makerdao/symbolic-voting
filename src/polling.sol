c90ccedb47b530e240951a9b216677cf62b97289
Joshua Levine <joshuapark95@gmail.com>
2019-06-13 18:06:58 -0600

    remove everything but the event emitter from the polling contract

--- src/polling.sol
+++ src/polling.sol
@@ -1,122 +1,43 @@
-pragma solidity ^0.4.24;
-
-import "ds-token/token.sol";
-import "ds-math/math.sol";
-import "ds-auth/auth.sol";
+pragma solidity >=0.5.0;
 
 contract PollingEvents {
     event PollCreated(
-        uint256 id, 
-        uint128 indexed numChoices, 
-        uint64 start,
-        uint64 end, 
-        address indexed creator, 
+        address indexed sender,
+        uint256 pollId,
+        uint128 indexed numChoices,
+        uint64 startBlock,
+        uint64 endBlock,
         string multiHash
     );
 
     event PollWithdrawn(
-        uint256 id, 
-        address indexed creator, 
-        uint256 timestamp
+        address indexed sender,
+        uint256 pollId,
+        uint256 blockNumber
     );
-    
+
     event Voted(
-        address indexed lad, 
-        uint256 indexed id, 
-        uint128 indexed pick, 
+        address indexed sender,
+        uint256 indexed pollId,
+        uint128 indexed pick,
         bytes logData
     );
 }
 
-// canonical event emitter 
-contract PollingEmitter is PollingEvents, DSNote {
-    mapping (address => uint256) public wards;
-    function rely(address guy) public note auth { wards[guy] = 1; }
-    function deny(address guy) public note auth { wards[guy] = 0; }
-    modifier auth { require(wards[msg.sender] == 1); _; }
+contract PollingEmitter is PollingEvents {
+    string constant public VERSION = "1.0.0";
+    uint256 public npoll;
 
-    function createPoll(uint256 id, uint128 numChoices, uint64 start, uint64 end, string multiHash) 
-        public view auth { emit PollCreated(id, numChoices, start, end, msg.sender, multiHash); }
+    function createPoll(uint128 numChoices, uint64 startBlock, uint64 endBlock, string memory multiHash)
+        public view {
+            emit PollCreated(msg.sender, npoll, numChoices, startBlock, endBlock, multiHash);
+            npoll++;
+    }
 
-    function vote(address lad, uint256 id, uint128 pick, bytes logData) 
-        public view auth { emit Voted(lad, id, pick, logData); }
+    function withdrawPoll(uint256 pollId)
+        public view { emit PollWithdrawn(msg.sender, pollId, block.number); }
 
-    function withdraw(uint256 id) 
-        public view auth { emit PollWithdrawn(id, msg.sender, now); }
+    function vote(uint256 pollId, uint128 pick, bytes memory logData)
+        public view { emit Voted(msg.sender, pollId, pick, logData); }
 }
 
-contract ResolveLike { 
-    function canSpeakFor(address, address) public view returns (bool);
-}
-
-// emitter access control; upgradable 
-contract PollingGuard is DSAuth, DSMath {
-    string constant public VERSION = "0.1.0-alpha";
-
-    PollingEmitter public polling;
-    address public       resolver;
-    uint256 public          npoll;
-    DSToken public            gov; 
-
-    mapping (uint256 => Poll) public polls;    
-
-    struct Poll {
-        uint64 start;
-        uint64 end;      
-        uint128 numChoices;
-        bool withdrawn;
-        address creator;
-    }
-
-    constructor(DSToken _gov, PollingEmitter _polling, address _resolver) 
-        public { gov = _gov; polling = _polling; resolver = _resolver; }
-
-    function createPoll(uint128 numChoices, uint64 delay, uint64 ttl, string multiHash) 
-        public auth returns (uint256) 
-    {
-        require(ttl > 0, "poll must have a valid voting period");
-        Poll storage poll = polls[npoll];
-        uint64 _start = uint64(add(delay, now ));
-        uint64 _end   = uint64(add(_start, ttl));
-
-        poll.numChoices = numChoices;
-        poll.creator    = msg.sender;
-        poll.start      = _start;
-        poll.end        = _end;
-
-        polling.createPoll(npoll, numChoices, _start, _end, multiHash);
-        return npoll++;
-    }
-
-    function vote(uint256 id, uint128 pick, bytes logData) 
-        public { _vote(msg.sender, id, pick, logData); }
-
-    function vote(address lad, uint256 id, uint128 pick, bytes logData) public {
-        require(ResolveLike(resolver).canSpeakFor(msg.sender, lad), 
-            "couldn't prove ownership of prospective address"
-        );
-        _vote(lad, id, pick, logData);
-    }
-
-    function _vote(address lad, uint256 id, uint128 pick, bytes logData) internal view {
-        Poll storage poll = polls[id];
-
-        require(id < npoll && !poll.withdrawn,  "id must be of a valid poll");
-        require(now >= poll.start && now <= poll.end, "id must be of an active poll");
-        require(gov.balanceOf(lad) > 0.005 ether, "voter must have more than 0.005 GOV");
-        require(pick <= poll.numChoices, "pick must be within the choice range");
-
-        polling.vote(lad, id, pick, logData);
-    }
-
-    function withdraw(uint256 id) public {
-        Poll storage poll = polls[id];
-
-        require(id < npoll && !poll.withdrawn,  "id must be of a valid poll");
-        require(poll.creator == msg.sender, "poll must be withdrawn by its creator");
-        require(poll.start > now, "poll can't be withdrawn after it has started");
-
-        poll.withdrawn = true;
-        polling.withdraw(id);
-    }
-}
--- src/polling.t.sol
+++ src/polling.t.sol
@@ -3,84 +3,24 @@ pragma solidity ^0.4.24;
 import "ds-test/test.sol";
 import "ds-token/token.sol";
 
-import {PollingSingleUseFab} from "./fab.sol";
-import {PollingGuard, PollingEmitter} from "./polling.sol";
-import {VoteProxyFactory, VoteProxy, DSChiefFab} from "./_vote-proxy-factory.sol";
+import {PollingEmitter} from "./polling.sol";
 
 contract Voter {
-    PollingGuard pollingGuard;
-    VoteProxyFactory voteProxyFactory;
-    
-    function setPolling(PollingGuard pollingGuard_) { pollingGuard = pollingGuard_; }
-
-    // for proxy voting tests -------------------------------------------------
-    function setProxyFactory(VoteProxyFactory _voteProxyFactory) 
-        { voteProxyFactory = _voteProxyFactory; }
-    function initiateLink(address hot) public { voteProxyFactory.initiateLink(hot); }
-    function approveLink(address cold) public returns (VoteProxy) { return voteProxyFactory.approveLink(cold); }
-    // ------------------------------------------------------------------------
-}
-
-contract Hevm {
-    function warp(uint256) public;
+    PollingEmitter pollingEmitter;
+    function setPolling(PollingEmitter pollingEmitter_) public { pollingEmitter = pollingEmitter_; }
 }
 
 contract PollingTest is DSTest {
     bytes constant LOG_DATA = new bytes(1);
 
-    PollingSingleUseFab pollingFab;
-    VoteProxyFactory voteProxyFactory;
-
-    PollingGuard pollingGuard;
     PollingEmitter pollingEmitter;
-    DSToken gov;
-    DSToken iou;
-    Hevm hevm;
-    Voter dan;
-    Voter eli;
-    Voter ned;
 
     function setUp() public {
-        // HEVM cheat -> set the block timestamp
-        hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
-        hevm.warp(100);
-
-        gov = new DSToken("GOV");
-        iou = new DSToken("IOU");
-        gov.mint(400 ether);
-
-        // from _vote-proxy-factory contracts imported for testing ------------
-        DSChiefFab dsChiefFab = new DSChiefFab();
-        voteProxyFactory = new VoteProxyFactory(dsChiefFab.newChief(gov, 5));
-        // --------------------------------------------------------------------
-
-        pollingFab = new PollingSingleUseFab(voteProxyFactory, gov);
-
-        dan = new Voter();
-        eli = new Voter();
-        ned = new Voter();
-        gov.transfer(dan, 100 ether);
-        gov.transfer(eli, 100 ether);
-        gov.transfer(ned, 100 ether);
-
-        address[] memory pollCreators = new address[](2);
-        pollCreators[0] = dan;
-        pollCreators[1] = eli;
-        (PollingEmitter pollingEmitter, PollingGuard pollingGuard) = pollingFab.newPolling(pollCreators);
-        dan.setPolling(pollingGuard); 
-        eli.setPolling(pollingGuard); 
-        ned.setPolling(pollingGuard);
-
-        // from _vote-proxy-factory contracts imported for testing ------------
-        dan.setProxyFactory(voteProxyFactory); 
-        eli.setProxyFactory(voteProxyFactory); 
-        ned.setProxyFactory(voteProxyFactory);
-        // --------------------------------------------------------------------
+        pollingEmitter = new PollingEmitter();
     }
 
-    ////////////////////////////////
-    ///////////// TODO /////////////
-    ////////////////////////////////
-
+    function test_create_poll() public {
+        pollingEmitter.createPoll(1, 1, 1, "");
+    }
 }
 
